c031b81ecca305e8921929d8b64786c6d29e17d0
Joel Semar <semarjt@gmail.com>
2016-12-14 06:52:00 -0600

    allow str_to_bool to take a bool

--- services/utils.py
+++ services/utils.py
@@ -214,6 +214,8 @@ def flatten(items):
 def str_to_bool(str):
     if str is None:
         return False
+    if str == True or str == False:
+        return str
     return str.lower() in ['1', 'true', 't', 'y', 'yes']
 
 
